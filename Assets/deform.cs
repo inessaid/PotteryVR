c3b189b7a45cd73463feee5d42085636d26b5f15
inessaid <43462511+inessaid@users.noreply.github.com>
2021-03-21 18:09:26 -0400

    Working with ball

--- Assets/deform.cs
+++ Assets/deform.cs
@@ -4,45 +4,65 @@ using UnityEngine;
 
 public class deform : MonoBehaviour
 {
+    //public Camera VRCamera;
+    public GameObject ball;
+    public float min;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(ball.transform.position,transform.position);
+        Debug.Log("Distance: " + distance);
+
+        if (distance < min)
+        {
+            Debug.Log("Should Deform");
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                BSoftBody hitBody = hit.transform.GetComponent<BSoftBody>();
+                if (hitBody)
+                    hitBody.DeformAtPoint(hit.point, hit.normal, 35f, 0.7f);
+            }
+        }
         
-    }
 
 
 
 
-  /*   void OnTriggerStay(Collider other)
-    {
-       // Debug.Log("contact**");
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        /*   void OnTriggerStay(Collider other)
+          {
+             // Debug.Log("contact**");
+              RaycastHit hit;
+              if (Physics.Raycast(transform.position, transform.forward, out hit))
+              {
+                  Debug.Log("Point of contact: " + hit.point);
+              }
+
+
+          }*/
+
+        /*void OnCollisionEnter(Collision collision)
         {
-            Debug.Log("Point of contact: " + hit.point);
-        }
-
-         
-    }*/
-
-    /*void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("contact**");
-        BSoftBody hitBody = hit.transform.GetComponent<BSoftBody>();
-        if (hitBody)
-            hitBody.DeformAtPoint(hit.point, hit.normal, 35f, 0.7f);
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Debug.DrawRay(contact.point, contact.normal, Color.white);
-        }
-    }*/
+            Debug.Log("contact**");
+            BSoftBody hitBody = hit.transform.GetComponent<BSoftBody>();
+            if (hitBody)
+                hitBody.DeformAtPoint(hit.point, hit.normal, 35f, 0.7f);
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                Debug.DrawRay(contact.point, contact.normal, Color.white);
+            }
+        }*/
 
 
 
     }
+}
