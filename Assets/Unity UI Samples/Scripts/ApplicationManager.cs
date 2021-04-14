cda1a635cd1c2e3874afa14967c05d9441e0b8f9
inessaid <43462511+inessaid@users.noreply.github.com>
2021-04-14 14:34:07 -0400

    enviornement switch

---
+++ Assets/Unity UI Samples/Scripts/ApplicationManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+	
+
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+	public void switchScene(string scene)
+    {
+		SceneManager.LoadScene(scene);
+    }
+}
